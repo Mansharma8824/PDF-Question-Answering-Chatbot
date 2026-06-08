c23ac041e6d712c193cf3f9b4c4159bc6aeb9c53
Manish Kumar <manishsharma9675800189@gmail.com>
2026-06-08 23:27:13 +0530

    add retriever and getting relevent chunks

---
+++ src/retrival.py
@@ -0,0 +1,15 @@
+
+from vector_store import load_vector_db
+
+def get_retreval(embedding_model):
+    vector_store = load_vector_db(
+        embedding_model= embedding_model
+    )
+    
+    retrival = vector_store.as_retriever(
+        search_type="similarity",
+        search_kwargs={"k": 3}
+    )
+    
+    return retrival
+
