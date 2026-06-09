ac985634386c9003a3cf8b913048a6bcc9be832b
Manish Kumar <manishsharma9675800189@gmail.com>
2026-06-09 17:58:57 +0530

    added basic sending request and geting response code using javascript and flask

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request,jsonify
+
 
 app = Flask(__name__)
 
@@ -7,6 +8,16 @@ def home():
     return render_template(
         "index.html"
     )
+
+@app.route("/chat", methods = ['POST'])
+def chat():
+    data = request.get_json()
+    
+    question = data['question']
+    return jsonify({
+        "answer": f"You asked: {question}"
+    })
+
 
 
 if __name__ == "__main__":
